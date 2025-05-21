b6d9cb722e866f05d07f3a3dd56161b947839de1
Asten <0xasten@gmail.com>
2025-05-21 21:04:51 +0800

    config accountAssociation

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,10 +4,13 @@ import { APP_URL } from '../../../lib/constants'
 export async function GET() {
   const farcasterConfig = {
     accountAssociation: {
-      header: '',
-      payload: '',
-      signature: '',
+      header:
+        'eyJmaWQiOjYzNDksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhlODNCRWUyQ2I0Rjk1QWY5NzhGQzY4REJkNjU0ZjBCQTE3ZEQ0ZWFjIn0',
+      payload: 'eyJkb21haW4iOiJtZW1lLWFpLWFzc2lzdGFudC5hcmNobGFiLnNwYWNlIn0',
+      signature:
+        'MHhjN2I2MmM0NGY4YWQ5M2RmYWYxZjQ1MGQ3NTg2M2Q4YTU0ZTk3Mzg1NzJlNDQxZGFjMTljMmQwNjQ2MWUxMDExNjZiYzhkN2NjNWFlMzlkOTk3ZjA5NTU2MGQ3ZjYwZjU0ODZjOTI3OThjNmFmYmNiYmJkMzY2MDQ5NGFiYTZhYzFj',
     },
+
     frame: {
       version: '1',
       name: 'Monad Meme AI Assistant',
